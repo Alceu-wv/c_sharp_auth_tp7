20b4bd6e79078c8ee4aa244e6be4f9bacd68178b
Alceu-wv <alceu.wv@gmail.com>
2023-02-23 21:38:27 -0300

    first-commit

---
+++ projeto_tp7/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace projeto_tp7.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
